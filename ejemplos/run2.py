4a1a5b4d8993b779c6e75f4df8d6c3705ef34572
renataa <116390061+reeenatamc@users.noreply.github.com>
2023-05-13 09:46:41 -0500

    2run2.py

--- ejemplos/run2.py
+++ ejemplos/run2.py
@@ -1,18 +1,32 @@
-"""
+# Crear dos objetos de la clase Gato
 
-"""
-# Crear dos objetos de la clase 02
+class Gato:
+    def __init__(self, nombre, edad, raza):
+        self.nombre = nombre
+        self.edad = edad
+        self.raza = raza
 
 # objeto 01
 
 # crear
 
+gato_1 = Gato("Whiskers", 2, "Siamés")
+
 # Presentar objeto; usar el método __st__
+
+print("Mi primer gato se llama", gato_1.nombre, "tiene", gato_1.edad, "años y es de raza", gato_1.raza)
 
 # objeto 02
 
 # crear ingresando valores por teclado
 
+nombre = input("Ingresa el nombre del segundo gato: ")
+edad = int(input("Ingresa la edad del segundo gato: "))
+raza = input("Ingresa la raza del segundo gato: ")
+
+gato_2 = Gato(nombre, edad, raza)
+
 # Presentar objeto; usar el método __st__
 
+print("Mi segundo gato se llama", gato_2.nombre, "tiene", gato_2.edad, "años y es de raza", gato_2.raza)
 
