374579b987ac97f62b91666636eda448c0ea9bc4
renataa <116390061+reeenatamc@users.noreply.github.com>
2023-05-13 09:44:38 -0500

    1run.py

--- ejemplos/run.py
+++ ejemplos/run.py
@@ -1,18 +1,26 @@
 """
-
+Crear dos objetos de la clase Auto
 """
-# Crear dos objetos de la clase 01
 
-# objeto 01
+class Auto:
+    def __init__(self, marca, modelo, año):
+        self.marca = marca
+        self.modelo = modelo
+        self.año = año
 
-# crear
+# Crear objeto 01
+auto_1 = Auto("Toyota", "Corolla", 2020)
 
-# Presentar objeto; usar el método __st__
+# Presentar objeto; usar el método __str__
+print("Mi primer auto es de marca: ", auto_1.marca)
 
-# objeto 02
+# Crear objeto 02 ingresando valores por teclado
+marca = input("Ingresa la marca del segundo auto: ")
+modelo = input("Ingresa el modelo del segundo auto: ")
+año = int(input("Ingresa el año del segundo auto: "))
+auto_2 = Auto(marca, modelo, año)
 
-# crear ingresando valores por teclado
-
-# Presentar objeto; usar el método __st__
+# Presentar objeto; usar el método __str__
+print("Mi segundo auto es de marca: ", auto_2.marca)
 
 
